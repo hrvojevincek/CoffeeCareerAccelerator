1a14a264c9f40715111cf922d0024036a6450d83
Arol Viñolas <arol@arol.dev>
2023-06-16 17:50:05 +0200

    fix(server): routes

--- apps/server/src/router.ts
+++ apps/server/src/router.ts
@@ -1,29 +1,35 @@
-//USER
+import { Router } from "express";
+const router = Router();
 
-router.post('/users', UsersController.createUser);
-router.get('/login', UsersController.loginUser);
+router.post("/users", UsersController.createUser);
+router.get("/users/login", UsersController.loginUser);
 
-router.get('/users/:userId', UsersController.getUser);
-router.get('/users/', UsersController.getUsers);
-router.put('/users/:userId', UsersController.updateUser);
-router.delete('/user/:userId', UserController.deleteUser);
+router.get("/users/:userId", UsersController.getUser);
+router.get("/users/", UsersController.getUsers);
+router.put("/users/:userId", UsersController.updateUser);
+router.delete("/user/:userId", UserController.deleteUser);
 
-router.post('/users/experience', UsersController.createExperience);
-router.get('/users/experience', UsersController.getExperience);
-router.put('/users/experience/:id', UsersController.updateExperience);
-router.delete('/users/experience/:id', UsersController.deleteExperience);
+router.post("/users/experiences", UsersController.createExperience);
+router.get("/users/experiences", UsersController.getExperience);
+router.put("/users/experiences/:id", UsersController.updateExperience);
+router.delete("/users/experiences/:id", UsersController.deleteExperience);
 
 //EMPLOYER
 
-router.post('/employer', EmployerController.createEmployer);
-router.get('/login', EmployerController.loginEmployer);
-router.get('/employer/', EmployerController.getEmployers);
-router.get('/employer/:employerId', EmployerController.getEmployer);
-router.put('/employer/:employerId', EmployerController.updateEmployer);
-router.delete('/employer/:employerId', EmployerController.deleteEmployer);
+router.post("/employers", EmployerController.createEmployer);
+router.get("/employers/login", EmployerController.loginEmployer);
+router.get("/employers/", EmployerController.getEmployers);
+router.get("/employers/:employerId", EmployerController.getEmployer);
+router.put("/employers/:employerId", EmployerController.updateEmployer);
+router.delete("/employers/:employerId", EmployerController.deleteEmployer);
 
-router.post('/employer/:employerId/job', EmployerController.createJob);
-router.put('/employer/:employerId/job/:jobId', EmployerController.updateJob);
-router.delete('/employer/:employerId/job/:jobId', EmployerController.deleteJob);
+router.post("/employer/:employerId/jobs", EmployerController.createJob);
+router.put("/employer/:employerId/jobs/:jobId", EmployerController.updateJob);
+router.delete(
+  "/employer/:employerId/jobs/:jobId",
+  EmployerController.deleteJob
+);
 
 // router.get('/job', jobController.getJobs); ????
+
+export default router;
