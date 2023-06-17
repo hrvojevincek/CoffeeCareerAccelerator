607be195952ac61bb09324b3d3a6dc17a9682298
hrvoje <vincekhrvoje@gmail.com>
2023-06-17 19:44:19 +0200

    client-react-tailwind-typesc

---
+++ apps/client/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-slate-300">
+      <h1 className="text-3xl font-bold text-black-600">COFFEE JOBS</h1>
+    </div>
+  );
+}
+
+export default App;
