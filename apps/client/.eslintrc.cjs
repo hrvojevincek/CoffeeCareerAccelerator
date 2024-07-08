1a0170880d8e9e566d8b3ba5d1fc9f236f6c955a
hrvoje vincek <vincekhrvoje@gmail.com>
2024-07-08 20:24:15 +0200

    lint and prettier update

--- apps/client/.eslintrc.cjs
+++ apps/client/.eslintrc.cjs
@@ -1,14 +1,48 @@
-module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
+const config = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+    tsconfigRootDir: __dirname,
+    sourceType: "module",
   },
-}
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "prettier",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:react-hooks/recommended",
+    "plugin:storybook/recommended",
+  ],
+  ignorePatterns: [
+    "tailwind.config.js",
+    "postcss.config.js",
+    "cypress/support/commands.js",
+  ],
+  rules: {
+    "@typescript-eslint/array-type": "off",
+    "@typescript-eslint/consistent-type-definitions": "off",
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        prefer: "type-imports",
+        fixStyle: "inline-type-imports",
+      },
+    ],
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-misused-promises": [
+      2,
+      {
+        checksVoidReturn: { attributes: false },
+      },
+    ],
+
+    "import/prefer-default-export": "off", // Allows named exports without default export
+    "no-console": "warn", // Warns about the use of console.log and similar functions
+    "no-param-reassign": "warn", // Warns against reassigning function parameters
+    "no-unused-vars": "warn", // Warns about unused variables (outside of TypeScript files)
+    "prefer-destructuring": "warn", // Encourages destructuring when possible
+    "no-underscore-dangle": "off", // Allows underscores in variable names
+  },
+};
+
+module.exports = config;
--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,54 +1,57 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 
-import JobDetailPage from "./pages/JobDetailPage";
-import CompanyPage from "./pages/CompanyPage";
-import JobsPage from "./pages/JobsPage";
-import UserPage from "./pages/UserPage";
-import { useState } from "react";
-import React from "react";
-import MainPage from "./pages/home/MainPage";
-import SignupPage from "./pages/auth/signup/SignupPage";
-import LoginPage from "./pages/auth/login/LoginPage";
-import Navbar from "./components/Navbar";
+import JobDetailPage from './pages/JobDetailPage';
+import CompanyPage from './pages/CompanyPage';
+import JobsPage from './pages/JobsPage';
+import UserPage from './pages/UserPage';
+import { useState } from 'react';
+import React from 'react';
+import MainPage from './pages/home/MainPage';
+import SignupPage from './pages/auth/signup/SignupPage';
+import LoginPage from './pages/auth/login/LoginPage';
+import Navbar from './components/Navbar';
 
 type UserContextType = [
-  user: User | null,
-  setUser: React.Dispatch<React.SetStateAction<User | null>>
+    user: User | null,
+    setUser: React.Dispatch<React.SetStateAction<User | null>>
 ];
 
 const initialContextState: UserContextType = [
-  null,
-  () => {
-    console.warn("setUser function not yet provided");
-  },
+    null,
+    () => {
+        console.warn('setUser function not yet provided');
+    },
 ];
 
 export const userContext =
-  React.createContext<UserContextType>(initialContextState);
+    React.createContext<UserContextType>(initialContextState);
 
 console.log(userContext);
 
 function App() {
-  const [user, setUser] = useState<User | null>(null);
+    const [user, setUser] = useState<User | null>(null);
 
-  return (
-    <>
-      <userContext.Provider value={[user, setUser]}>
-        <Navbar>
-          <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="/signup" element={<SignupPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/jobs/" element={<JobsPage />} />
-            <Route path="/jobs/:id" element={<JobDetailPage />} />
-            <Route path="/user/:id/*" element={<UserPage />} />
-            <Route path="/employer/:id/*" element={<CompanyPage />} />
-            <Route path="/employer" element={<CompanyPage />} />
-          </Routes>
-        </Navbar>
-      </userContext.Provider>
-    </>
-  );
+    return (
+        <>
+            <userContext.Provider value={[user, setUser]}>
+                <Navbar>
+                    <Routes>
+                        <Route path='/' element={<MainPage />} />
+                        <Route path='/signup' element={<SignupPage />} />
+                        <Route path='/login' element={<LoginPage />} />
+                        <Route path='/jobs/' element={<JobsPage />} />
+                        <Route path='/jobs/:id' element={<JobDetailPage />} />
+                        <Route path='/user/:id/*' element={<UserPage />} />
+                        <Route
+                            path='/employer/:id/*'
+                            element={<CompanyPage />}
+                        />
+                        <Route path='/employer' element={<CompanyPage />} />
+                    </Routes>
+                </Navbar>
+            </userContext.Provider>
+        </>
+    );
 }
 
 export default App;
